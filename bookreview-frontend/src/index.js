950389c0b6e197003d8fec33fbec7303878a76c6
stabassum <sumu1993@gmail.com>
2021-07-13 11:20:34 -0400

    all code up to date

--- bookreview-frontend/src/index.js
+++ bookreview-frontend/src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <Provider>
+  <React.StrictMode>
     <App />
-  </Provider>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
