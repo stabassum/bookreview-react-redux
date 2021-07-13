2a4fd0d56d414ee197a80dbbc84791997cd9a6d6
stabassum <sumu1993@gmail.com>
2021-07-12 21:22:10 -0400

    users_controller.rb file updated

--- bookreview-backend/app/controllers/users_controller.rb
+++ bookreview-backend/app/controllers/users_controller.rb
@@ -1,23 +1,42 @@
 class UsersController < ApplicationController
 
+    # GET /users
     def index
+        users = User.all
+        render json: users
+        # render json: UserSerializer.new(users)
     end
 
+    # GET /users/:id
     def show
+        user = User.find_by(id: params[:id])
+        render json: user
     end
 
+    # POST /users
     def create
+        user = User.new(user_params)
+        if user.save
+            session[:user_id] = user.id
+            render json: user #, status: :created
+        else
+            render json: user.errors #, status: :unprocessable_entity
+        end
     end
 
     def update
+
     end
 
     def destroy
+        user = User.find_by(id: params[:id])
+        user.destroy
     end
 
     private
 
     def user_params
+        params.require(:user).permit(:username, :email, :password)
     end
 
 end
