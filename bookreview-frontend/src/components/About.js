5811ec4d69a050ef5a82af52234bdf85321ec2d8
stabassum <sumu1993@gmail.com>
2021-07-13 10:51:55 -0400

    About.js updated

---
+++ bookreview-frontend/src/components/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container, Divider } from 'semantic-ui-react'
+ 
+
+const About = () => {
+
+  return (
+    <div>
+      <Divider />
+        <p>About</p>
+    </div>
+  )
+
+}
+
+
+export default About;
