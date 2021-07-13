4d477c14462f8866cab1c39ed3a526d7e6bfa432
stabassum <sumu1993@gmail.com>
2021-07-13 10:51:07 -0400

    Home.js updated

---
+++ bookreview-frontend/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Divider } from 'semantic-ui-react'
+
+const Home = () => {
+  return(
+
+  <div className="Home">
+     <Divider />
+    <p>Home Page</p>
+  </div>
+)
+}
+
+export default Home;
