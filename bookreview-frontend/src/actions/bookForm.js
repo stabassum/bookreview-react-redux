f589fe435d9a6988b3287a76128456c226ff8f8c
stabassum <sumu1993@gmail.com>
2021-07-13 14:09:50 -0400

    bookForm.js action file updated

---
+++ bookreview-frontend/src/actions/bookForm.js
@@ -0,0 +1,11 @@
+export const updateBookFormData = bookFormData => {
+    return {
+        type: 'UPDATED_DATA'
+    }
+}
+
+export const resetBookForm = () => {
+    return {
+        type: 'RESET_TOY_FORM'
+    }
+}
