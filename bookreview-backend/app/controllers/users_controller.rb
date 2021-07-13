fb076a8adf35160d0aca65e194d49404c03bd71c
stabassum <sumu1993@gmail.com>
2021-07-12 20:54:03 -0400

    UsersController.rb  updated

---
+++ bookreview-backend/app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+
+    def index
+    end
+
+    def show
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def user_params
+    end
+
+end
