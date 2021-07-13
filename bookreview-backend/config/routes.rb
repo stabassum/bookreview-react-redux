cef7b9bd812e084698e4ec8c429910c9b2669533
stabassum <sumu1993@gmail.com>
2021-07-12 21:33:15 -0400

    routes.rb file updated

--- bookreview-backend/config/routes.rb
+++ bookreview-backend/config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # need to implement nested routes
+
+  # get '/login' => "sessions#new"
+
+  post '/sessions' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  post '/signup' => 'users#create'
+
+  # need to fix nested routing problem
+
+  resources :users
+
+  resources :books
+
+  resources :book_ownerships
+
 end
