89f9136ecdec66d9c479630752ec2b3b9a330e1f
stabassum <sumu1993@gmail.com>
2021-07-13 00:15:31 -0400

    Footer.js file updated in bookreview-frontend

---
+++ bookreview-frontend/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="Footer">
+            <p className="FooterContent">Book Review © Copyright 2021</p>
+        </div>
+    )
+}
+
+export default Footer;
