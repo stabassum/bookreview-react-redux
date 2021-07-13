fbcc12961fc0465ee0b0aa69b7caa52f099ca85d
stabassum <sumu1993@gmail.com>
2021-07-13 10:53:46 -0400

    bookreview-front end files updated in App.js

--- bookreview-frontend/src/App.js
+++ bookreview-frontend/src/App.js
@@ -21,8 +21,8 @@ class App extends React.Component {
 
           <Switch>
             <Route exact path="/" component={Home}/>
-            <Route path="/books" component={Books}/>
-            <Route path="/myStuff" component={MyStuff}/>
+            {/* <Route path="/books" component={Books}/>
+            <Route path="/myStuff" component={MyStuff}/> */}
             <Route path="/about" component={About}/>
           </Switch>
 
