d77e869b72140f6dccf247970e1974a0f88fa19f
stabassum <sumu1993@gmail.com>
2021-07-13 00:50:50 -0400

    NavBar.js file updated

--- bookreview-frontend/src/components/NavBar.js
+++ bookreview-frontend/src/components/NavBar.js
@@ -1,1 +1,37 @@
 import React from 'react';
+
+import { Link } from 'react-router-dom'
+import { Menu } from 'semantic-ui-react';
+
+const NavBar = () => {
+
+    return (
+
+        <div clanssName="nav">
+            <Menu>
+
+                <Link to='' className='item'>
+                    Home
+                </Link>
+
+                <Link to='' className='book'>
+                    View All Book Reviews
+                </Link>
+
+                <Link to=''> 
+                    View My Book Reviews
+                </Link>
+
+                <Link to=''>
+                    About
+                </Link>
+
+
+            </Menu>
+        </div>
+
+    )
+
+}
+
+export default NavBar;
