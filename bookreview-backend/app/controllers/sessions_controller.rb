15dcd3fd58eda44db88cfb97349812e03fd0dc0d
stabassum <sumu1993@gmail.com>
2021-07-12 20:55:49 -0400

    BooksController file updated

---
+++ bookreview-backend/app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+def BooksController < ApplicationController
+
+    def index
+    end
+
+    def show
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+end
