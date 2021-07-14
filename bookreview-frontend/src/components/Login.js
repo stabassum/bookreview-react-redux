07db50efa2905096f87c6b8fdcb3858449e48676
stabassum <sumu1993@gmail.com>
2021-07-13 21:51:53 -0400

    updated Login.js file

--- bookreview-frontend/src/components/Login.js
+++ bookreview-frontend/src/components/Login.js
@@ -4,17 +4,27 @@ import { Divider, Icon} from 'semantic-ui-react';
 export default class Login extends React.Component {
     render(){
         return(
+            <div>
+            <Divider />
+
             <form>
             
             <div className="ui input">
             
-            <input placeholder="user name" value={loginFormData.username} name="username" type="text" />
-            <input placeholder="password" value={loginFormData.password} name="password" type="text" /><br/><br/>
+            <label>Username</label>
+            <input placeholder="user name"  name="username" type="text" />
+           
+            <br/>
+            <label>Password</label>
+            <input placeholder="password"  name="password" type="text" /><br/><br/>
             
             <button className="button button-login" type="submit" value="Log In" > <Icon name="sign in" /> Log in </button>
             
+            <br/>
+
              </div>
             </form>
+            </div>
         )
     }
 }
