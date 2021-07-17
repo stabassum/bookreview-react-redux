385bc26fc22ca8bcdfe05e24b0085771f125f0f0
stabassum <sumu1993@gmail.com>
2021-07-16 20:48:55 -0400

    About.js updated

--- bookreview-frontend/src/components/About.js
+++ bookreview-frontend/src/components/About.js
@@ -7,7 +7,9 @@ const About = () => {
         <p>About</p>
 
         <br/>
-        <p>This application will allow users to add reviews about the books they have read.</p>
+        <p>"Book Review" will allo users to keep track of books that a reader feels important to keep track of.</p>
+        <p>Not only will a user keep track of a book, but they can insert their review for the book as well in summary format.</p>
+        <p>To view only the books and reviews of the user, the user must create an account, otherwise user must login/create an account.</p>
     </div>
   )
 
