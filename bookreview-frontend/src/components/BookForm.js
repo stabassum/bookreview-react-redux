7cfe00ee40398c15dfb6e2f3dc5bd955115fe66e
stabassum <sumu1993@gmail.com>
2021-07-24 13:50:32 -0400

    BookForm.js updated

--- bookreview-frontend/src/components/BookForm.js
+++ bookreview-frontend/src/components/BookForm.js
@@ -19,9 +19,10 @@ export default class BookForm extends Component {
 
         <h4>Add Book</h4>  
 
-        <Form>
+        {/* <Form>
 
             <Form.Group widths='equal'>
+                
                 <Form.Input fluid label='Title: ' placeholder='Name'/>
                 <Form.Input fluid label='Author: ' placeholder='Author'/>
                 <Form.Input fluid label='Review: ' placeholder='Review'/>
@@ -30,7 +31,28 @@ export default class BookForm extends Component {
 
             <button>Add Book Review</button>
 
-        </Form>
+        </Form> */}
+
+
+            <form>
+            
+                <div className="ui input">
+
+                <label>Title: </label>
+                <input placeholder="Name"  name="Name" type="text" />
+            
+                <br/>
+                <br/>
+                <label>Author: </label>
+                <input placeholder="Author"  name="Author" type="text" /><br/><br/>
+
+                <label>Review: </label>
+                <input placeholder="Review"  name="Review" type="text" /><br/><br/>
+
+                <button className="button button-add-bookreview" type="submit" value="Add Book Review" > <Icon name="" /> Add Book Review </button>
+
+                </div>
+            </form>
 
         </div>
 
