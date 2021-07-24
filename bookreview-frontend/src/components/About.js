d95bf8346a22495e700b5581b484a23722c36fd8
stabassum <sumu1993@gmail.com>
2021-07-24 14:19:59 -0400

    all code up to date

--- bookreview-frontend/src/components/About.js
+++ bookreview-frontend/src/components/About.js
@@ -7,9 +7,9 @@ const About = () => {
         <h4>About</h4>
 
         <br/>
-        <p>"Book Review" will allo users to keep track of books that a reader feels important to keep track of.</p>
-        <p>Not only will a user keep track of a book, but they can insert their review for the book as well in summary format.</p>
-        <p>To view only the books and reviews of the user, the user must create an account, otherwise user must login/create an account.</p>
+        <p>"Book Review" will allo users to keep track of books that a reader feels important to keep track of. 
+        Not only will a user keep track of a book, but they can insert their review for the book as well in summary format. 
+        To view only the books and reviews of the user, the user must create an account, otherwise user must login/create an account.</p>
     </div>
   )
 
--- bookreview-frontend/src/components/Home.js
+++ bookreview-frontend/src/components/Home.js
@@ -9,10 +9,10 @@ const Home = () => {
     <h4>Home Page</h4>
     <br/>
 
-    <p>Some of the books you have read in your school or personal, would you like to keep track of them and review them?</p>
-    <p>"Book Review" is the perfect place to keep track of your book memories and readings so you can go back and see them in a set location!</p>
+    <p>Some of the books you have read in your school or personal, would you like to keep track of them and review them? 
+      "Book Review" is the perfect place to keep track of your book memories and readings so you can go back and see them in a set location!</p>
     <br/>
-    <p></p>
+  
     
   </div>
 )
